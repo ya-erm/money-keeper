055862e40c36566eb0f396fa915290f68fa25c6a
Iaroslav Ermilov <>
2022-11-21 14:07:33 +0400

    Fix groupId cookie

--- src/routes/groups/+page.server.ts
+++ src/routes/groups/+page.server.ts
@@ -50,7 +50,19 @@ const selectGroup: Action = async ({ request, locals, cookies }) => {
 
   locals.groupId = groupId;
 
-  cookies.set('groupId', groupIdString);
+  cookies.set('groupId', groupIdString, {
+    // send cookie for every page
+    path: '/',
+    // server side only cookie so you can't use `document.cookie`
+    httpOnly: true,
+    // only requests from same site can send cookies
+    // https://developer.mozilla.org/en-US/docs/Glossary/CSRF
+    sameSite: 'strict',
+    // only sent over HTTPS in production
+    secure: process.env.NODE_ENV === 'production',
+    // set cookie to expire after a month
+    maxAge: 60 * 60 * 24 * 30,
+  });
 
   throw redirect(302, `${routes.groups.path}/${groupId}`);
 };
