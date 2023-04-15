5c368449b9568a42b78bc01ecdf03f04347dbb4e
Yaroslav Ermilov <25526713+ya-erm@users.noreply.github.com>
2023-04-15 16:23:06 +0300

    Add v2 links and revert disable ssr

--- src/routes/settings/encryption/+page.svelte
+++ src/routes/settings/encryption/+page.svelte
@@ -11,4 +11,12 @@
   <ListSwitchItem title="Encryption" checked={false} />
   <ListSelectItem title="Public key" value="******" />
   <EnableEncryptionModal bind:opened />
+
+  <div class="mt-2 flex-col gap-1">
+    <h3>v2 links</h3>
+    <a href="/v2/login">/v2/login</a>
+    <a href="/v2/categories">/v2/categories</a>
+    <a href="/v2/accounts/list">/v2/accounts/list</a>
+    <a href="/v2/transactions">/v2/transactions</a>
+  </div>
 </div>
