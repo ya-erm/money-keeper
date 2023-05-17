8a1beb248c6e91369a3a3ee8819e35db7b7d46d4
Yaroslav Ermilov <25526713+ya-erm@users.noreply.github.com>
2023-05-17 12:43:31 +0300

    Version 1.0.1

--- src/routes/settings/+page.server.ts
+++ src/routes/settings/+page.server.ts
@@ -23,7 +23,7 @@ export const load: PageServerLoad = async ({ locals, depends }) => {
       where: { id: groupId },
     }));
 
-  const version = '0.1.0';
+  const version = '1.0.1';
 
   return { user, groups, group, version };
 };
