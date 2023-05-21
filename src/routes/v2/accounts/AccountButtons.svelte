be02bc2300818479129302ac28e668f50669dd33
Yaroslav Ermilov <25526713+ya-erm@users.noreply.github.com>
2023-05-21 12:47:24 +0300

    Change add account button icon

--- src/routes/v2/accounts/AccountButtons.svelte
+++ src/routes/v2/accounts/AccountButtons.svelte
@@ -7,7 +7,7 @@
 <div class="flex">
   <HeaderButton icon="mdi:format-list-bulleted" label={$translate('accounts.list')} href={route('accounts.list')} />
   <HeaderButton
-    icon="mdi:plus"
+    icon="mdi:credit-card-plus-outline"
     label={$translate('accounts.create_account')}
     href={route('accounts.list') + '?action=create'}
   />
