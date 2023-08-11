bf602d5dabeaa5b57f8be6160cce90f02df1527b
Yaroslav Ermilov <25526713+ya-erm@users.noreply.github.com>
2023-08-11 13:15:52 +0300

    Add dayjs locale

--- src/lib/translate/index.ts
+++ src/lib/translate/index.ts
@@ -1,7 +1,13 @@
-import { storable } from '$lib/storable';
 import { format, getLocaleFromNavigator, init, locale } from 'svelte-i18n';
 import { derived, type Readable } from 'svelte/store';
+
+import dayjs from 'dayjs';
+import 'dayjs/locale/ru';
+
+import { storable } from '$lib/storable';
+
 import type { Locales, Messages } from './messages';
+
 import './en';
 import './ru';
 
@@ -24,12 +30,15 @@ type MessageFormatter = (id: Messages, options?: FormatOptions) => string;
 
 export const translate = format as Readable<MessageFormatter>;
 
-export const languages: { [key in Locales]: { name: string; icon: string } } = {
-  'ru-RU': { name: 'Русский', icon: 'circle-flags:ru' },
-  'en-US': { name: 'English', icon: 'circle-flags:gb' },
+export const languages: { [key in Locales]: { name: string; icon: string; dayjs: string } } = {
+  'ru-RU': { name: 'Русский', icon: 'circle-flags:ru', dayjs: 'ru' },
+  'en-US': { name: 'English', icon: 'circle-flags:gb', dayjs: 'en' },
 };
 
 export const activeLocale = storable<Locales>((initialLocale as Locales) ?? 'ru-RU', 'locale');
-activeLocale.subscribe((value) => locale.set(value));
+activeLocale.subscribe((value) => {
+  locale.set(value);
+  dayjs.locale(languages[value]?.dayjs);
+});
 
 export const activeLocaleName = derived(activeLocale, (value) => languages[value]?.name ?? languages['en-US'].name);
