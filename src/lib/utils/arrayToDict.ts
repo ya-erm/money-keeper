10244d5f3cb4e61e62991db506836bc67aefa925
Yaroslav Ermilov <25526713+ya-erm@users.noreply.github.com>
2023-11-18 05:30:55 +0300

    Add some doc to utils

--- src/lib/utils/arrayToDict.ts
+++ src/lib/utils/arrayToDict.ts
@@ -1,4 +1,18 @@
-export function arrayToDict<T, S>(array: T[], map: (item: T) => S, key: (item: S) => string | undefined) {
+/**
+ * Converts an array to a dictionary using the provided mapping functions.
+ *
+ * @template T The type of the items in the array.
+ * @template S The type of the items in the resulting dictionary.
+ * @param {T[]} array The array to convert to a dictionary.
+ * @param {(item: T) => S} map The mapping function to transform each item in the array.
+ * @param {(item: S) => string | undefined} key The function to extract the key from each mapped item.
+ * @returns {Map<string, S>} The resulting dictionary.
+ */
+export function arrayToDict<T, S>(
+  array: T[],
+  map: (item: T) => S,
+  key: (item: S) => string | undefined,
+): Map<string, S> {
   const items = new Map<string, S>();
   array.forEach((item) => {
     const mapped = map(item);
--- src/lib/utils/filterNotEmpty.ts
+++ src/lib/utils/filterNotEmpty.ts
@@ -1,3 +1,9 @@
+/**
+ * Filters out `null` and `undefined` values from an array.
+ *
+ * @param array The array to filter.
+ * @returns A new array with non-empty values.
+ */
 export function filterNotEmpty<T>(array?: T[]): NonNullable<T>[] {
   return (array?.filter((item) => item !== null && item !== undefined) as NonNullable<T>[]) ?? [];
 }
