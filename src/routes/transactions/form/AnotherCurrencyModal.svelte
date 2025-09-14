fd4790235db864c53c895f7045d05ac95bcf3a68
Yaroslav Ermilov <25526713+ya-erm@users.noreply.github.com>
2025-09-14 15:54:21 +0400

    Show last used currencies in another currency modal

--- src/routes/transactions/form/AnotherCurrencyModal.svelte
+++ src/routes/transactions/form/AnotherCurrencyModal.svelte
@@ -2,21 +2,34 @@
   import Button from '@ya-erm/svelte-ui/Button';
   import Input from '@ya-erm/svelte-ui/Input';
   import Modal from '@ya-erm/svelte-ui/Modal';
+  import Tags from '@ya-erm/svelte-ui/Tags';
 
-  import { memberSettingsStore, membersService } from '$lib/data';
+  import { memberSettingsStore, membersService, operationsStore } from '$lib/data';
   import { translate } from '$lib/translate';
 
   $: settings = $memberSettingsStore;
 
   export let opened: boolean;
   export let anotherCurrency: string | null;
+  $: anotherCurrencyInputValue = anotherCurrency;
+
+  const lastUsedCurrencies = (() => {
+    const currencies = new Set<string>();
+    for (const operation of $operationsStore) {
+      if (operation.anotherCurrency) {
+        currencies.add(operation.anotherCurrency);
+        if (currencies.size === 4) break;
+      }
+    }
+    return Array.from(currencies).map((currency) => ({ id: currency, title: currency }));
+  })();
 </script>
 
-<Modal bind:opened>
+<Modal bind:opened width={18}>
   <form
     class="flex-col gap-1"
-    on:submit|preventDefault={async (e) => {
-      anotherCurrency = new FormData(e.currentTarget).get('another-currency')?.toString() ?? null;
+    on:submit|preventDefault={async () => {
+      anotherCurrency = anotherCurrencyInputValue;
       if (anotherCurrency !== settings?.lastAnotherCurrency) {
         await membersService.updateSettings({ lastAnotherCurrency: anotherCurrency });
       }
@@ -25,10 +38,13 @@
   >
     <Input
       label={$translate('transactions.another_currency')}
-      value={anotherCurrency}
+      bind:value={anotherCurrencyInputValue}
       name="another-currency"
       clearable
     />
+    {#if lastUsedCurrencies.length > 0}
+      <Tags tags={lastUsedCurrencies} selected={[]} onChange={(id) => (anotherCurrencyInputValue = id)} readOnly />
+    {/if}
     <div class="grid-col-2 gap-1">
       <Button
         color="secondary"
@@ -36,6 +52,7 @@
         onClick={() => {
           opened = false;
           anotherCurrency = null;
+          anotherCurrencyInputValue = null;
         }}
       />
       <Button text={$translate('common.save')} color="primary" type="submit" />
